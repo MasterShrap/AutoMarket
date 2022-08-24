c96a530f1cf4154124e55e27077e564204c294c0
Ramazon0220 <mrkolobok0220@gmail.com>
2022-08-24 17:31:55 +0300

    Project Ready

---
+++ Automarket/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Automarket.Models;
+
+namespace Automarket.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
